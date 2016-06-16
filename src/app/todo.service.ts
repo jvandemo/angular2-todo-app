f2d936abb840334eff120444f212857ccd4a0394
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2016-06-16 17:40:39 +0200

    feat(todo): add TodoService

---
+++ src/app/todo.service.ts
@@ -0,0 +1,55 @@
+import {Injectable} from '@angular/core';
+import {Todo} from './todo';
+
+@Injectable()
+export class TodoService {
+
+  // Placeholder for last id so we can simulate
+  // automatic incrementing of id's
+  lastId: number = 0;
+
+  // Placeholder for todo's
+  todos: Todo[] = [];
+
+  constructor() {
+  }
+
+  // Simulate POST /todos
+  addTodo(todo: Todo): TodoService {
+    if (!todo.id) {
+      todo.id = ++this.lastId;
+    }
+    this.todos.push(todo);
+    return this;
+  }
+
+  // Simulate DELETE /todos/:id
+  deleteTodoById(id: number): TodoService {
+    this.todos = this.todos
+      .filter(todo => todo.id !== id);
+    return this;
+  }
+
+  // Simulate PUT /todos/:id
+  updateTodoById(id: number, values: Object = {}): Todo {
+    let todo = this.getTodoById(id);
+    if (!todo) {
+      return null;
+    }
+    Object.assign(todo, values);
+    return todo;
+  }
+
+  // Simulate GET /todos
+  getAllTodos(): Todo[] {
+    return this.todos;
+  }
+
+  // Simulate GET /todos/:id
+  getTodoById(id: number): Todo {
+    return this.todos
+      .filter(todo => todo.id === id)
+      .pop();
+  }
+
+}
