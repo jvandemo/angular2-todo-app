76caf29efb70572a226f59f003321c6b4d39cccb
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2016-06-16 17:40:21 +0200

    test(todo): add unit tests for Todo class

---
+++ src/app/todo.spec.ts
@@ -0,0 +1,26 @@
+/* tslint:disable:no-unused-variable */
+
+import {
+  beforeEach, beforeEachProviders,
+  describe, xdescribe,
+  expect, it, xit,
+  async, inject
+} from '@angular/core/testing';
+import {Todo} from './todo';
+
+describe('Todo', () => {
+
+  it('should create an instance', () => {
+    expect(new Todo()).toBeTruthy();
+  });
+
+  it('should accept values in the constructor', () => {
+    let todo = new Todo({
+      title: 'hello',
+      complete: true
+    });
+    expect(todo.title).toEqual('hello');
+    expect(todo.complete).toEqual(true);
+  });
+
+});
