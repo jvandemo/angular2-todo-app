4beaf0f48e3dd8ce322224daaa4f293a1a449caa
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2016-06-17 12:27:29 +0200

    feat(app): load todo-app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,9 +15,4 @@ describe('App: Todo', () => {
       inject([AppComponent], (app: AppComponent) => {
     expect(app).toBeTruthy();
   }));
-
-  it('should have as title \'app works!\'',
-      inject([AppComponent], (app: AppComponent) => {
-    expect(app.title).toEqual('app works!');
-  }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { TodoAppComponent } from './todo-app';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [TodoAppComponent]
 })
 export class AppComponent {
-  title = 'app works!';
 }
