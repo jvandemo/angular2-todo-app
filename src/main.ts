0af4abb79448fd2bd8b983cabbb9cb0dac75ef9c
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2016-06-17 12:26:26 +0200

    feat(main): add provider for TodoService

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import { TodoService } from './app/todo.service';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  TodoService
+]);
 
